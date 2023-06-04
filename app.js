549182029f85e92e1accac5ae0976f23ee48de90
crownVic0285 <vpcorona85@gmail.com>
2023-06-04 16:39:45 -0700

    clear search box and make ajax call

--- app.js
+++ app.js
@@ -16,3 +16,18 @@ function addGif(res) {
         $gifArea.append($newCol);
     }
 }
+
+$("form").on("submit", async function(evt) {
+  evt.preventDefault();
+
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+    }
+  });
+  addGif(response.data);
+});
